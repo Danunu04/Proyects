3e0015aca1e8e68b23288693ebe627a863f48c3a
Danunu04 <dperelmuter25@gmail.com>
2025-05-20 17:29:42 -0300

    Agrego archivos

---
+++ Practicas/Observer/Observer/EstacionMeteorologica.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    public class EstacionMeteorologica: Iestacion
+    {
+        private string nombre;
+        public EstacionMeteorologica(string nom)
+        {
+            this.nombre = nom;
+        }
+        public void actualizar (float Temperatura)
+        {
+            Console.WriteLine($"[{nombre}] Temperatura registrada: {Temperatura}°C");
+        }
+    }
+}
